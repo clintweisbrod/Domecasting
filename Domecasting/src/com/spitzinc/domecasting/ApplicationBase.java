9dafdfc08ac9be2960afc4e5dbb774a1329663ca
Clint Weisbrod <cweisbrod@spitzinc.com>
2017-10-29 19:08:33 -0400

    More indentation corrections.

--- Domecasting/src/com/spitzinc/domecasting/ApplicationBase.java
+++ Domecasting/src/com/spitzinc/domecasting/ApplicationBase.java
@@ -12,84 +12,84 @@ import javax.swing.UnsupportedLookAndFeelException;
 public abstract class ApplicationBase implements WindowListener
 {
 	protected static ApplicationBase singleInstance;
-	
+
 	static
 	{
 		singleInstance = null;
 	}
-	
+
 	abstract protected void createUIElements();
-	
+
 	public static ApplicationBase inst()
 	{
 		return singleInstance;
 	}
-	
+
 	protected ApplicationBase()
 	{
-    	singleInstance = this;
-    	
-    	// Set application Look & Feel to Windows
-    	try {
+		singleInstance = this;
+
+		// Set application Look & Feel to Windows
+		try {
 			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
 		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
 				| UnsupportedLookAndFeelException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    	
-    	createUIElements();
+
+		createUIElements();
 	}
-	
+
 	public void positionFrame(JFrame frame)
 	{
 		// Center application on screen
 		Dimension prefSize = frame.getPreferredSize();
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		int x = (screenSize.width - prefSize.width) / 2;
-        int y = (screenSize.height - prefSize.height) / 2;
-        frame.setLocation(x, y);
+		int y = (screenSize.height - prefSize.height) / 2;
+		frame.setLocation(x, y);
 	}
-	
+
 	@Override
 	public void windowActivated(WindowEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void windowClosed(WindowEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void windowClosing(WindowEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void windowDeactivated(WindowEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void windowDeiconified(WindowEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void windowIconified(WindowEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void windowOpened(WindowEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
