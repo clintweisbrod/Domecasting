90016a00cb2af8dbd9f6caa65e042938003e8f07
cweisbrod <cweisbrod@spitzinc.com>
2017-10-30 18:44:44 -0400

    Thread running client that handles communication with server.

---
+++ Domecasting/src/com/spitzinc/domecasting/client/ServerConnectionThread.java
@@ -0,0 +1,42 @@
+package com.spitzinc.domecasting.client;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import com.spitzinc.domecasting.TCPConnectionHandlerThread;
+
+public class ServerConnectionThread extends TCPConnectionHandlerThread
+{
+	protected InputStream in;
+	protected OutputStream out;
+	
+	public ServerConnectionThread()
+	{
+		super(null, null);
+	}
+	
+	public void run()
+	{
+		final String hostName = "localhost";
+		final int port = 80;
+		socket = connectToHost(hostName, port);
+		if (socket != null)
+		{
+			try
+			{
+				in = socket.getInputStream();
+				out = socket.getOutputStream();
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			if ((in != null) && (out != null))
+			{
+				// Allocate byte buffer to handle comm
+				byte[] buffer = new byte[16*1024];
+			}
+		}
+	}
+}
